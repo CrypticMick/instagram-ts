3dbe9808f8ed800eef42360c4694b6dd9758148f
Mickey Martinez <mickey@echobind.com>
2022-11-03 16:39:21 -0700

    theme colors and fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import colors from "./src/theme/colors";
+import fonts from "./src/theme/fonts";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: colors.primary, fontSize: fonts.size.lg }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
