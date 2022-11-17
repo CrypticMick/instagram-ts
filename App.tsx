dbbfc3d9775275fb27535da54083a344f7a0fe52
Mickey Martinez <mickey@echobind.com>
2022-11-17 12:16:38 -0800

    build header for posts

--- App.tsx
+++ App.tsx
@@ -1,25 +1,70 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import colors from "./src/theme/colors";
 import fonts from "./src/theme/fonts";
 
+import { Entypo } from "@expo/vector-icons";
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={{ color: colors.primary, fontSize: fonts.size.lg }}>
-        Open up App.js to start working on your app!
-      </Text>
+    <SafeAreaView style={styles.post}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg",
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>AmongUs Blue</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+
+      {/* Content */}
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+        }}
+        style={styles.image}
+      />
+
+      {/* Footer */}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  post: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  header: {
+    flexDirection: "row",
+    padding: 10,
     alignItems: "center",
-    justifyContent: "center",
+  },
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: fonts.weight.bold,
+    color: colors.black,
+  },
+  threeDots: {
+    // moves 3 dots to the right
+    marginLeft: "auto",
   },
 });
 
