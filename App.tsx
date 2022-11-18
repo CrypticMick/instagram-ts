ddbb62c524a2fd2972838a72643183d44f62996b
Mickey Martinez <mickey@echobind.com>
2022-11-18 12:30:15 -0800

    style footer and comments on post

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import colors from "./src/theme/colors";
 import fonts from "./src/theme/fonts";
 
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, AntDesign, Ionicons, Feather } from "@expo/vector-icons";
 
 const App = () => {
   return (
@@ -16,7 +16,7 @@ const App = () => {
           }}
           style={styles.userAvatar}
         />
-        <Text style={styles.userName}>AmongUs Blue</Text>
+        <Text style={styles.userName}>AmongUsBlue</Text>
         <Entypo
           name="dots-three-horizontal"
           size={16}
@@ -33,6 +33,67 @@ const App = () => {
       />
 
       {/* Footer */}
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name={"hearto"}
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{ marginLeft: "auto" }}
+            color={colors.black}
+          />
+        </View>
+
+        {/* Likes */}
+        <Text style={styles.text}>
+          Liked by{" "}
+          <Text style={{ fontWeight: fonts.weight.bold }}>mortysmith</Text> and{" "}
+          <Text style={{ fontWeight: fonts.weight.bold }}>50 others</Text>
+        </Text>
+
+        {/* Post description */}
+        <Text style={styles.text}>
+          <Text style={{ fontWeight: fonts.weight.bold }}>AmongUsBlue</Text> A
+          beautiful morning for a hike. It's starting to get chilly out,
+          thankfully I brought my Summit 2022 thermos. Kept my coffee blazing
+          hot!
+        </Text>
+
+        {/* Comments */}
+        <Text style={{ color: "grey" }}>View all 15 comments</Text>
+        <View style={styles.comment}>
+          <Text style={styles.commentText}>
+            <Text style={{ fontWeight: fonts.weight.bold }}>mortysmith</Text>{" "}
+            Looks gorgeous, just look at it. Gorgeous.
+          </Text>
+          <AntDesign
+            name={"hearto"}
+            size={14}
+            style={styles.icon}
+            color={colors.black}
+          />
+        </View>
+
+        {/* Time since posted */}
+        <Text style={{ color: "grey" }}>1w</Text>
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -65,6 +126,27 @@ const styles = StyleSheet.create({
   threeDots: {
     // moves 3 dots to the right
     marginLeft: "auto",
+  },
+  footer: {
+    padding: 10,
+  },
+  iconContainer: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  icon: {
+    margin: 5,
+  },
+  comment: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  commentText: {
+    flex: 1,
+    lineHeight: 18,
+  },
+  text: {
+    lineHeight: 18,
   },
 });
 
